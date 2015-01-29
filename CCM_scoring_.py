6019e90e17835ae4c08fd10c36f8551256c46f47
Colin Maxwell <cs.maxwell@gmail.com>
2015-01-29 09:48:03 -0500

    Min max work. SWING interface works

--- CCM_scoring_.py
+++ CCM_scoring_.py
@@ -3,7 +3,9 @@ import ij.IJ
 import ij.gui
 from ij import IJ, ImagePlus, WindowManager
 from ij.gui import Roi, Overlay, GenericDialog
-from java.awt.event import KeyEvent, KeyAdapter, ActionListener
+from java.awt.event import KeyEvent, KeyAdapter, ActionListener, WindowAdapter
+from javax.swing import JScrollPane, JPanel, JComboBox, JLabel, JFrame, JButton, JFormattedTextField, JTextField
+from java.awt import Color, GridLayout
 
 # Keycode 78 is "n"
 
@@ -22,17 +24,18 @@ class GridReader:
         self.loadSourceImage()
         self.openImage = ImagePlus()
         self.openNext(auto=True)
-
-    def setMinMax(self, minVal, maxVal):
-        self.min, self.max = minVal, maxVal
-        self.openImage.getProcessor().setMinAndMax(self.min, self.max)
-        self.openImage.updateAndDraw()
-
-    def getMin(self):
-        return self.min
-
-    def getMax(self):
-        return self.max
+    
+    def loadSourceImage( self ):
+        head, tail = os.path.split( self.fp )
+        fn, ext = os.path.splitext( tail )
+        fnSplit = fn.split("_")
+        if len(fnSplit) != 2:
+            raise ValueError("File name is not formatted properly")
+        imgPath = os.path.join(head, fnSplit[0] + ".tif")
+        img = ImagePlus(imgPath)
+        if img is None:
+            raise ValueError("Couldn't find the image")
+        self.sourceImage = img
     
     def openNext( self, auto=False ):
         self.openImage.close()
@@ -50,6 +53,17 @@ class GridReader:
         self.openImage.show()
         return self.openImage
     
+    def setMinAndMax(self, minVal, maxVal):
+        self.min, self.max = minVal, maxVal
+        self.openImage.getProcessor().setMinAndMax(self.min, self.max)
+        self.openImage.updateAndDraw()
+
+    def getMin(self):
+        return self.min
+
+    def getMax(self):
+        return self.max
+    
     def setContrast(self, auto=False):
         if auto:
             IJ.run(self.openImage, "Enhance Contrast", "saturated=0.35")
@@ -58,55 +72,118 @@ class GridReader:
         else:
             self.openImage.getProcessor().setMinAndMax(self.min, self.max)
             self.openImage.updateAndDraw()
-    
-    def loadSourceImage( self ):
-        head, tail = os.path.split( self.fp )
-        fn, ext = os.path.splitext( tail )
-        fnSplit = fn.split("_")
-        if len(fnSplit) != 2:
-            raise ValueError("File name is not formatted properly")
-        imgPath = os.path.join(head, fnSplit[0] + ".tif")
-        img = ImagePlus(imgPath)
-        if img is None:
-            raise ValueError("Couldn't find the image")
-        self.sourceImage = img
 
-
-def doSomething(imp, keyEvent):
-    """ A function to react to key being pressed on an image canvas. """
-    IJ.log("clicked keyCode " + str(keyEvent.getKeyCode()) + " on image " + str(imp))
-    # Prevent further propagation of the key event:
-    keyEvent.consume()
+# def dialogMinMax(gridObject):
+#     gd = GenericDialog('Set new min and max')
+#     gd.addNumericField('Min: ', gridObject.getMin(), 0)
+#     gd.addNumericField('Max: ', gridObject.getMax(), 0)
+#     minField = gd.getNumericFields().get(0)
+#     maxField = gd.getNumericFields().get(1)
+#     gd.showDialog()
+#     if not gd.wasCanceled():
+#         newMin = gd.getNextNumber()
+#         newMax = gd.getNextNumber()
+#         gridObject.setMinAndMax(newMin, newMax)
  
-class ListenToKey(KeyAdapter):
-    def __init__(self, fxn):
-        KeyAdapter.__init__(self)
-        self.fxn = fxn
-    def keyPressed(self, event):
-        imp = event.getSource().getImage()
-        self.fxn(imp, event)
 
-def addKeyListeners(listen):
-    for imp in map(WindowManager.getImage, WindowManager.getIDList()):
-        win = imp.getWindow()
-        if win is None:
-            continue
-        canvas = win.getCanvas()
-        # Remove existing key listeners
-        kls = canvas.getKeyListeners()
-        map(canvas.removeKeyListener, kls)
-        # Add our key listener
-        canvas.addKeyListener(listen)
-        # Optionally re-add existing key listeners
-        # map(canvas.addKeyListener, kls)
 
-listener = ListenToKey(doSomething)
 
+################### GUI
+
+class ChangedMin(ActionListener):
+    def __init__(self, field):
+        self.field = field
+    def actionPerformed(self, event):
+        global plateGrid
+        newValue = self.field.getValue()
+        currentMax = plateGrid.getMax()
+        plateGrid.setMinAndMax(newValue, currentMax)
+
+class ChangedMax(ActionListener):
+    def __init__(self, field):
+        self.field = field
+    def actionPerformed(self, event):
+        global plateGrid
+        newValue = self.field.getValue()
+        currentMin = plateGrid.getMin()
+        plateGrid.setMinAndMax(currentMin, newValue)
+
+class NextImage(ActionListener):
+  def actionPerformed(self, event):
+      global plateGrid
+      global listener
+      global frame
+      plateGrid.openNext()
+      frame.setVisible(True)
+
+class Closing(WindowAdapter):
+    def windowClosing(self,e):
+        #WindowManager.closeAllWindows()
+        global plateGrid
+        plateGrid.sourceImage.close()
+        plateGrid.openImage.close()
+        pass
+
+class NextImageListener(KeyAdapter):
+    def keyPressed(self, keyEvent):
+        global plateGrid
+        global frame
+        IJ.log("clicked keyCode " + str(keyEvent.getKeyCode()))
+        keyPressed = keyEvent.getKeyCode()
+        if keyPressed == 78: # This is the 'n' key
+            plateGrid.openNext()
+            frame.setVisible(True)
+        if keyPressed == 10: # This is the 'enter' key
+            plateGrid.openNext()
+            frame.setVisible(True)
+        # Prevent further propagation of the key event:
+        keyEvent.consume()
 
 plateGrid = GridReader(fp = "/Users/cm/Desktop/CCM_scorer/plate-001_plate1")
+
+all = JPanel()
+layout = GridLayout(4, 1)
+all.setLayout(layout)
+
+all.add( JLabel("  ") )
+button = JButton("Next image")
+button.addActionListener( NextImage() )
+button.addKeyListener(NextImageListener())
+all.add( button )
+
+all.add( JLabel("Min") )
+textField1 = JFormattedTextField(68)
+textField1.addActionListener( ChangedMin(textField1) )
+#textField1.addKeyListener(listener)
+all.add(textField1)
+
+all.add( JLabel("Max") )
+textField2 = JFormattedTextField(130)
+textField2.addActionListener( ChangedMax(textField2) )
+#textField2.addKeyListener(listener)
+all.add(textField2)
+
+all.add( JLabel("Score :") )
+textField3 = JFormattedTextField( float(0.0) )
+textField3.addActionListener( NextImage() )
+#textField3.addKeyListener( NextImageListener() )
+all.add(textField3)
+
+frame = JFrame("CCM scoring")
+frame.getContentPane().add(JScrollPane(all))
+frame.pack()
+frame.addWindowListener( Closing() )
+frame.setVisible(True)
+
+
+
+
+
 #plateGrid.setMinMax(76, 136)
 #imp = plateGrid.openNext()
-addKeyListeners(listener)
+
+
+#dialogMinMax(plateGrid)
 
 
 # from java.awt import Color
@@ -115,17 +192,4 @@ addKeyListeners(listener)
 # from ij import Menus
 # from ij.gui import GenericDialog
 
-# #commands = [c for c in ij.Menus.getCommands().keySet()]
-# # Above, equivalent list as below:
-# gd = GenericDialog('Set new min and max')
-# gd.addNumericField('Min: ', grid.min)
-# gd.addNumericField('Max: ', grid.max)
-# minField = gd.getNumericFields().get(0)
-# maxField = gd.getNumericFields().get(1)
-# # prompt.setForeground(Color.red)
 
-# prompt.addTextListener(TypeListener())
-# gd.showDialog()
-# if not gd.wasCanceled():
-#     newMin = gd.getNextNumber()
-#     newMax = gd.getNextNumber()
